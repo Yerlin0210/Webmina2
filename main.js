cfcc867cff834fba3b222e41e58ebac1d7e98522
Yerlin0210 <97493157+Yerlin0210@users.noreply.github.com>
2022-01-11 04:27:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+
+  ScrollReveal().reveal('.Showcase');
+ScrollReveal().reveal('.News-Cards', { delay: 500 });
+ScrollReveal().reveal('.Baner-numero-uno', { delay: 500 });
+ScrollReveal().reveal('.Baner-numero-dos', { delay: 500 });
